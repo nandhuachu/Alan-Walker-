dee96c1626cab99511a21400fc9281a888972441
Venom <67759177+nandhuachu@users.noreply.github.com>
2023-02-07 13:36:33 +0530

    🙂

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -523,7 +523,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ident, req, file_id = query.data.split("#")
         if BUTTON_LOCK:
             if int(req) not in [query.from_user.id, 0]:
-                return await query.answer(BUTTON_LOCK_TEXT.format(query=query.from_user.first_name), show_alert=True)             
+                return await query.answer(BUTTON_LOCK_TEXT.format(query=query.from_user.first_name), show_alert=False)             
         files_ = await get_file_details(file_id)
         if not files_:
             return await query.answer('No such file exist.')
