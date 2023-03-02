8a37820059c2a6091184230dcf8355d09af0c222
Venom <67759177+nandhuachu@users.noreply.github.com>
2023-03-02 22:06:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from pyrogram import types
 from datetime import datetime
 from pytz import timezone
 from pyrogram.errors import BadRequest, Unauthorized
-from plugins import web_server, __license__, __copyright__
+from plugins.__init__ import web_server
 from aiohttp import web
 
 # Get logging configurations
