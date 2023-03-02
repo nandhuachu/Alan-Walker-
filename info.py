be65f562ffd05d869041e85da118ee81f6e51aee
Venom <67759177+nandhuachu@users.noreply.github.com>
2023-03-02 22:50:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ WELCOM_PIC = environ.get("WELCOM_PIC", "")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hai {user}\nwelcome to {chat}")
 PMFILTER = environ.get('PMFILTER', "True")
 G_FILTER = bool(environ.get("G_FILTER", True))
-BUTTON_LOCK = environ.get("BUTTON_LOCK", "False")
+BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
 
 # FSUB
 auth_channel = environ.get('AUTH_CHANNEL')
